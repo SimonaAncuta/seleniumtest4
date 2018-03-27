5a057b5c00f1e74e0665153e18daf3ae1ff3b8e6
SimonaAncuta <simona.ancuta@ymail.com>
2018-03-27 10:17:04 +0300

    First commit

--- src/test/java/org/fasttrackit/TestBase.java
+++ src/test/java/org/fasttrackit/TestBase.java
@@ -3,9 +3,11 @@ package org.fasttrackit;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -22,6 +24,32 @@ public class TestBase {
 
         driver.get(AppConfig.getSiteUrl());
     }
+
+
+
+
+    protected void waitForPageToLoad(long timeout) {
+        long waited = 0;
+        long pauseMillis = 500;
+        do {
+            try {
+                Thread.sleep(pauseMillis);
+            } catch (InterruptedException e) {
+                System.out.println("Wait process interrupted.");
+            }
+            waited += pauseMillis;
+        }
+        while (waited <= timeout && !((JavascriptExecutor) driver)
+                .executeScript("return document.readyState")
+                .equals("complete"));
+    }
+
+    protected <T> T initElements(Class<T> pageObjectClass) {
+        waitForPageToLoad(10000);
+
+        return PageFactory.initElements(driver, pageObjectClass);
+    }
+
 
     @After
     public void teardown() {
